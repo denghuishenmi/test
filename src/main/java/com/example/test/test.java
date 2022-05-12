89d3fec495ef88d4fceeae32bd7e71b15a3958f2
denghuishenmi <zxc464267111@foxmail.com>
2022-05-12 21:22:39 +0800

    第二次提交

---
+++ src/main/java/com/example/test/test.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+/**
+ * @author denghuishenmi
+ * @create 2022-05-12 21:21
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("第二版本");
+    }
+}
