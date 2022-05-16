8df268c36318089124be2f59bccdb81dee3c139d
白书灬 <66932310+denghuishenmi@users.noreply.github.com>
2022-05-17 01:01:01 +0800

    Update test.java

--- src/main/java/com/example/test/test.java
+++ src/main/java/com/example/test/test.java
@@ -8,5 +8,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("第二版本");
         System.out.println("第二版本的1分支");
+        System.out.println("更新1分支，需要拉取到本地");
     }
 }
